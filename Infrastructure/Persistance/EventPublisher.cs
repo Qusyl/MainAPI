ae46ebdcaa28c0405486fa42468cc670f7c60e4a
Qusyl <95533550+Qusyl@users.noreply.github.com>
2026-05-01 16:26:06 +0300

    EventPublisher ( MediatR )

--- Infrastructure/Persistance/EventPublisher.cs
+++ Infrastructure/Persistance/EventPublisher.cs
@@ -1,14 +1,45 @@
 ﻿using Application.Interface;
 using Domain;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Persistance
 {
+    /// <summary>
+    /// Аналог MediatR - оповещение ( notification ) для событий
+    /// </summary>
     public class EventPublisher : IEventPublisher
     {
-        public Task PublishAsync(IDomainEvent @event, CancellationToken cts = default)
+        private readonly IServiceProvider _serviceProvider;
+
+        private readonly ILogger<EventPublisher> _logger;
+        public EventPublisher(IServiceProvider serviceProvider, ILogger<EventPublisher> logger)
         {
-           //Mediatr сделать
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        public async Task PublishAsync(IDomainEvent @event, CancellationToken cts = default)
+        {
+           var eventType = @event.GetType();
+
+           if(eventType == null)
+            {
+                _logger.LogError("Event type is not found", eventType);
+                return;
+            }
+
+           var handlerType = typeof(IHandler<>).MakeGenericType(eventType);
+            if (handlerType == null)
+            {
+                _logger.LogError("Handler type is not found", handlerType);
+                return;
+            }
+
+            var handlers = _serviceProvider.GetServices(handlerType);
+            var tasks = handlers.Cast<IHandler<IDomainEvent>>().Select(h => h.HandleAsync(@event, cts));
+
+            await Task.WhenAll(tasks);
 
         }
     }
